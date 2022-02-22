4d64b9c50b2568f35cc63930ed4bcc0867f35982
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-22 22:15:47 +0500

    Show properties func

--- script.js
+++ script.js
@@ -30,42 +30,66 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const minMax = function (...numbs) {
-  const min = Math.min(...numbs);
-  const max = Math.max(...numbs);
-  console.log(`Min: ${min}, Max: ${max}`);
-};
-minMax(1, 2, 3, 4, 5);
+// const minMax = function (...numbs) {
+//   const min = Math.min(...numbs);
+//   const max = Math.max(...numbs);
+//   console.log(`Min: ${min}, Max: ${max}`);
+// };
+// minMax(1, 2, 3, 4, 5);
 
-const func = function (...numbers) {
-  const devided = [];
-  const notDevided = [];
-  for (let i = 0; i <= numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-      devided.push(numbers[i]);
-    } else {
-      notDevided.push(numbers[i]);
+// const func = function (...numbers) {
+//   const devided = [];
+//   const notDevided = [];
+//   for (let i = 0; i <= numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//       devided.push(numbers[i]);
+//     } else {
+//       notDevided.push(numbers[i]);
+//     }
+//   }
+//   console.log(`Devided: ${devided} ,not Devided: ${notDevided}`);
+// };
+// func(1, 2, 3, 4, 5);
+
+// const reverse = function (...numbs) {
+//   const reversed = numbs.reverse();
+//   console.log(reversed);
+// };
+// reverse(1, 2, 3, 4, 5);
+
+// const max = function (big, small) {
+//   const check = big > small ? big : small;
+//   console.log(check);
+// };
+// max(2, 3);
+
+// const checkSpeed = function (speed) {
+//   const point = Math.floor((speed - 70) / 5);
+//   if (point < 12) console.log(point);
+//   else console.log('Guvohnoma olib qoyiladi');
+// };
+// checkSpeed(135);
+
+const sum = function (limit) {
+  let sumAll = 0;
+  for (let i = 0; i <= limit; i++) {
+    if (i % 3 === 0 || i % 5 === 0) sumAll += i;
+  }
+  return sumAll;
+};
+console.log(sum(10));
+
+let car = {
+  model: 'Toyota',
+  make: 'Estima',
+  year: 2016,
+  price: 5000,
+};
+const showProp = function (obj) {
+  for (const i of Object.values(obj)) {
+    if (typeof i === 'string') {
+      console.log(i);
     }
   }
-  console.log(`Devided: ${devided} ,not Devided: ${notDevided}`);
 };
-func(1, 2, 3, 4, 5);
-
-const reverse = function (...numbs) {
-  const reversed = numbs.reverse();
-  console.log(reversed);
-};
-reverse(1, 2, 3, 4, 5);
-
-const max = function (big, small) {
-  const check = big > small ? big : small;
-  console.log(check);
-};
-max(2, 3);
-
-const checkSpeed = function (speed) {
-  const point = Math.floor((speed - 70) / 5);
-  if (point < 12) console.log(point);
-  else console.log('Guvohnoma olib qoyiladi');
-};
-checkSpeed(135);
+showProp(car);
